63c508d3f127b28b160bf001c115dcb7e6168b13
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-01-30 10:14:45 +0100

    first commit

---
+++ src/main/java/com/BackendPoke/BackendPoke/BackendPokeApplication.java
@@ -0,0 +1,13 @@
+package com.BackendPoke.BackendPoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BackendPoke/BackendPoke/BackendPokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackendPoke.BackendPoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
