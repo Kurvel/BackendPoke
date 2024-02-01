ad2e74c8dfbac7a9e02cf4974b1dbe608b024898
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-02-01 16:09:47 +0100

    WIP PATCH

--- src/main/java/com/BackendPoke/BackendPoke/api/controller/PokemonController.java
+++ src/main/java/com/BackendPoke/BackendPoke/api/controller/PokemonController.java
@@ -1,6 +1,7 @@
 package com.BackendPoke.BackendPoke.api.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,8 +32,9 @@ public class PokemonController {
 
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/pokemon")
-    public Optional<Pokemon> getPokemon(@RequestParam("id") int id) {
+    public Optional<Pokemon> getPokemon(@RequestParam("pokemonId") int id) {
         System.out.println("Pokemon: " + id);
+        // TODO skapa metod för att hämta med pokemonId
         return pokemonRepository.findById(id);
     }
 
@@ -57,14 +60,31 @@ public class PokemonController {
 
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @DeleteMapping("/pokemon")
-   public ResponseEntity<String> deletePokemon(@RequestParam("id") int id) {
-        System.out.println("delete: " + id);
+   public ResponseEntity<String> deletePokemon(@RequestParam("pokemonId") int pokemonId) {
+        System.out.println("delete: " + pokemonId);
         
-        // Assuming the deletion is successful
-        pokemonRepository.deleteById(id);
+        pokemonRepository.deletePokemon(pokemonId);
+        // pokemonRepository.deleteById(pokemonId);
+        
 
-        // Return a JSON response
-        String response = "{\"message\": \"Deleted:\", \"id\": " + id + "}";
+        
+        String response = "{\"message\": \"Deleted:\", \"id\": " + pokemonId + "}";
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    @PatchMapping("/pokemon")
+    public ResponseEntity<?> notice(@RequestParam("pokemonId") int pokemonId, @RequestBody Pokemon updatedpokemon) {
+        System.out.println("Notis: " + pokemonId);
+        Optional<Pokemon> optionalPokemon = pokemonRepository.findByPokemonId(pokemonId);
+        if(optionalPokemon.isPresent()) {
+            Pokemon changeNotice = optionalPokemon.get();
+            changeNotice.setNotice(updatedpokemon.getNotice());
+            pokemonRepository.save(changeNotice);
+            return ResponseEntity.ok(Map.of("message", "OK"));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", "Fel"));
+        }
+        
+    }    
 }
